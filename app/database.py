32d45fc280d4ccbe1c9c973eda1179dd2f6038d3
DataLunatic69 <amansingh2002.ap@gmail.com>
2025-09-17 22:22:29 +0530

    updating database file

--- app/database.py
+++ app/database.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
+from app.config import config
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = str(config.DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
